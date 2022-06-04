581f9d4630f0d741f08afd67ed022a43475961b7
JulianoPiovesanFroner <104043010+JulianoPiovesanFroner@users.noreply.github.com>
2022-06-04 15:24:47 -0300

    Add files via upload

---
+++ tip_calculator.py
@@ -0,0 +1,17 @@
+while True:
+    conta = float(input("\nInforme o valor total do consumo: R$ "))
+    pessoas = int(input("Informe o total de pessoas: "))
+    if pessoas < 1:
+        print("Valor invalido")
+        break
+    percentualGorjeta = int(input("Informe o percentual do servico entre 0 e 100: "))
+    if percentualGorjeta < 0 or percentualGorjeta > 100:
+        print("Valor invalido")
+        break
+    gorjeta = conta * (percentualGorjeta / 100)
+    total = conta + gorjeta
+    round(total,2)
+    pagamento = total / pessoas
+    round(pagamento, 2)
+    print(f"O valor total da conta, com taxa de servico, sera de R$ {total}")
+    print(f"\nDividindo a conta por {pessoas} pessoa(s), cada pessoa devera pagar R$ {pagamento}")
---
+++ verificador_de_idade.py
@@ -0,0 +1,11 @@
+while True:
+    idade = int(input("Informe a idade: "))
+    if idade < 0:
+        print("Idade invalida")
+        break
+    elif idade > 18 and idade < 70:
+        print("Tem obrigacao de votar.")
+    elif (idade > 16 and idade < 18) or idade > 70:
+        print("Nao tem obrigacao de votar.")
+    else:
+        print("Nao tem direito ao voto.")
